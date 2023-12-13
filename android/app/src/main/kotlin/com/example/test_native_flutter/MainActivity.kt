e4a0b6211466a216eee7bcda6b96a0ca83e7c783
Kavinda Lochana <kaviduljayasekara@gmail.com>
2023-12-13 23:50:39 +0530

    get battery level

--- android/app/src/main/kotlin/com/example/test_native_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_native_flutter/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.test_native_flutter
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -21,6 +29,13 @@ class MainActivity: FlutterActivity() {
                 } else {
                     result.error("UNAVAILABLE", "Message from Kotlin side is empty", null)
                 }
+            } else if (call.method == "btLevel") {
+                val batLevel = getBatteryLevel()
+                if (batLevel != -1) {
+                    result.success(batLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
             } else {
                 result.notImplemented()
             }
@@ -28,9 +43,25 @@ class MainActivity: FlutterActivity() {
     }
 
 
-
+    // message to send to the flutter side
     private fun sendMessage(): String {
-        return "Hello I am Kotlin"
+        return "The battery level was coming from the Kotlin side."
+    }
+
+
+    // get device battery level
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
     }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Native Test',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Native Data Test'),
+      home: const MyHomePage(title: 'Check your battery level'),
     );
   }
 }
@@ -30,19 +30,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _kotText = 'Nothing yet';
+  String _kotText = 'Tap the button';
+  String _batteryLevel = '!';
   static const platform = MethodChannel('main_channel');
 
+// main method to get data from kotlin side
   Future<void> _getNativeMessage() async {
     String nativeMessage;
+    String nativeBatLevel;
     try {
       nativeMessage = await platform.invokeMethod('dataFlow');
+      final batResult = await platform.invokeMethod<int>('btLevel');
+      nativeBatLevel = '$batResult %';
     } on PlatformException catch (e) {
       nativeMessage = 'Failed to get native data: ${e.message}';
+      nativeBatLevel = 'Failed to get battery data: ${e.message}';
     }
 
     setState(() {
       _kotText = nativeMessage;
+      _batteryLevel = nativeBatLevel;
     });
   }
 
@@ -51,13 +58,35 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          textAlign: TextAlign.center,
+        ),
       ),
-      body: Center(child: Text(_kotText)),
+      body: Container(
+          width: double.infinity,
+          color: Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_kotText),
+              Card(
+                margin: const EdgeInsets.all(15.0),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    _batteryLevel,
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: _getNativeMessage,
         tooltip: 'Get data',
-        child: const Icon(Icons.get_app),
+        child: const Icon(Icons.battery_alert_sharp),
       ),
     );
   }
